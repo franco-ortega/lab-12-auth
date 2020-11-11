85a15b939217c968d4e3c55a425dc6b5847dfb00
Franco Ortega <francopdx@gmail.com>
2020-11-11 10:00:32 -0800

    change animals.js to todos.js and update create-tables, drop-tables, load-seed-data

--- data/todos.js
+++ data/todos.js
@@ -1,16 +1,16 @@
 module.exports = [
   {
-    id: 1,
+    
     chore: 'make breakfast',
     completed: false
   },
   {
-    id: 2,
+    
     chore: 'make lunch',
     completed: false
   },
   {
-    id: 3,
+    
     chore: 'make dinner',
     completed: false
   }
