116cb0aacd586ecd1237976ea6502ac0f321ee23
Franco Ortega <francopdx@gmail.com>
2020-11-11 09:55:04 -0800

    initial setup with bootstrap and new todos data

---
+++ data/animals.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    chore: 'make breakfast',
+    completed: false
+  },
+  {
+    id: 2,
+    chore: 'make lunch',
+    completed: false
+  },
+  {
+    id: 3,
+    chore: 'make dinner',
+    completed: false
+  }
+];
